8b2908bb26e8c5bcc168147582821135f6fc0d78
neelwalse <117557589+nwalse@users.noreply.github.com>
2025-05-23 17:00:28 -0700

    medium link updates

--- src/lib/projectData.ts
+++ src/lib/projectData.ts
@@ -28,7 +28,7 @@ export const projects: Array<{
         title: "Sea Otter Savvy",
         description: "A convenient app in order to share sea otter pictures, learn about marine life observation spots, and report disturbances to the proper authorities, with the goal of improving sea otter awareness through photography.",
         imageSrc: "projects/sos.svg",
-        mediumLink: "https://medium.com/@ucscblueprint/case-study-sea-otter-savvy-mobile-app-2085dffb53b1#id_token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjY2MGVmM2I5Nzg0YmRmNTZlYmU4NTlmNTc3ZjdmYjJlOGMxY2VmZmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIyMTYyOTYwMzU4MzQtazFrNnFlMDYwczJ0cDJhMmphbTRsamRjbXMwMHN0dGcuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDI2MDI1MzcxODc3NzYxNjUwMjAiLCJoZCI6InVjc2MuZWR1IiwiZW1haWwiOiJud2Fsc2VAdWNzYy5lZHUiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmJmIjoxNzQ3NzA4OTc3LCJuYW1lIjoiTmVlbCBXYWxzZSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS9BQ2c4b2NMSGR4U2pYd19KNGs2RloyZTY5R1B3bWV2cThwZkVWbW5ndjNHMVJCUWdBM0ZINUE9czk2LWMiLCJnaXZlbl9uYW1lIjoiTmVlbCIsImZhbWlseV9uYW1lIjoiV2Fsc2UiLCJpYXQiOjE3NDc3MDkyNzcsImV4cCI6MTc0NzcxMjg3NywianRpIjoiYjg0Mjc1YmI1Nzk0Yzg4Yjg5OTM3N2Y0YzllZGEyMmFhYzc3ZjNhNyJ9.XujUrLNFFqhBH1S6pTX73mQF5JkhdtVfrHYtAowWMIPtgKlK7EdEVaeqhG0mjpUbVnqr_5UD6gox0fVOKqpmLgMYlUWg-qbsE9u7mz0UJGwsLSpm7IppCHyrLaXkz8cd-ceceS4A2InZCNsIBYp54xma4lvM_O9xUlAbuX26NnXhBk3xu8iqFv47tQev1H2rItfWzyGMxCmDQaLqg7ekZg0EgTmxJk9OIttjVh8e6Q1kvDRSVvU-vhVOs6iJ36o12HM6-wUoTe2lhxHuKpGifWAg0IpwUFvnMSxlMy7dG0wbTLK3zHWaxeH4kWXDL1MYZb4dHvwR7BW8UISZsdyhGA",
+        mediumLink: "https://medium.com/@ucscblueprint/case-study-sea-otter-savvy-mobile-app-2085dffb53b1",
         demoLink: "https://apps.apple.com/gb/app/sea-otter-savvy/id6503718590",
         year: [2023, 2024],
     },
@@ -43,6 +43,7 @@ export const projects: Array<{
         description: "A simple event and volunteer tracking app designs for senior citizens to improve the event coordination process and prevent communication mistakes.",
         imageSrc: "projects/scmac.svg",
         demoLink: "https://apps.apple.com/us/app/santa-cruz-mountain-art-center/id6550900077",
+        mediumLink: "https://medium.com/@ucscblueprint/case-study-santa-cruz-mountains-art-center-scmac-mobile-app-5bfe312ea247",
         year: [2023, 2024, 2025],
 
     },
